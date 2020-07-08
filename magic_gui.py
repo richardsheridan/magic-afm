550f78ba4afeec242ff04f380ae03923566916e1
Richard Sheridan <richard.sheridan@gmail.com>
2020-07-08 19:19:58 -0400

    Revise about info display

--- magic_gui.py
+++ magic_gui.py
@@ -621,12 +621,15 @@ async def about_task(root):
     top.wm_title(f"About {app_name}")
     message = tk.Message(top, text=__short_license__)
     message.pack()
-    thread_disp_var = tk.StringVar(top)
-    thread_disp_label = ttk.Label(top, textvariable=thread_disp_var)
-    thread_disp_label.pack()
-    thread_disp_var2 = tk.StringVar(top)
-    thread_disp_label2 = ttk.Label(top, textvariable=thread_disp_var2)
-    thread_disp_label2.pack()
+    thread_cpu = tk.StringVar(top)
+    thread_cpu_label = ttk.Label(top, textvariable=thread_cpu)
+    thread_cpu_label.pack()
+    thread_default = tk.StringVar(top)
+    thread_default_label = ttk.Label(top, textvariable=thread_default)
+    thread_default_label.pack()
+    gc_count = tk.StringVar(top)
+    gc_count_label = ttk.Label(top, textvariable=gc_count)
+    gc_count_label.pack()
     opts = dict(mode="indeterminate", maximum=80, length=300)
     timely_trio_pbar = ttk.Progressbar(top, **opts)
     timely_trio_pbar.pack()
@@ -651,20 +654,21 @@ async def about_task(root):
             t = t + interval / 1000
             await trio.sleep_until(t)
 
-    async def thread_poller_task():
+    async def state_poller_task():
         while True:
-            thread_disp_var.set("CPU-bound threads:" + repr(async_tools.cpu_bound_limiter).split(",")[1][:-1])
-            thread_disp_var2.set(
+            t = trio.current_time()
+            thread_cpu.set("CPU-bound threads:" + repr(async_tools.cpu_bound_limiter).split(",")[1][:-1])
+            thread_default.set(
                 "Default threads:" + repr(trio.to_thread.current_default_thread_limiter()).split(",")[1][:-1]
             )
-            await trio.sleep(interval / 1000)
+            await trio.sleep_until(t+interval / 1000)
 
     # run using tcl event loop
     tk_pbar.start(interval)
     # run using trio
     async with trio.open_nursery() as nursery:
         top.protocol("WM_DELETE_WINDOW", nursery.cancel_scope.cancel)
-        nursery.start_soon(thread_poller_task)
+        nursery.start_soon(state_poller_task)
         nursery.start_soon(pbar_runner)
         nursery.start_soon(pbar_runner_timely)
     top.destroy()
