0d60a1b988db6d0137aec7abdd98c1113e5606ee
Richard Sheridan <richard.sheridan@gmail.com>
2020-07-08 19:42:43 -0400

    black

--- async_tools.py
+++ async_tools.py
@@ -40,48 +40,55 @@ def make_cancel_poller():
     return poll_for_cancel
 
 
-async def convert_ardf(ardf_path, conv_path=r'X:\Data\AFM\Cypher\ARDFtoHDF5.exe', force=False, pbar=None):
+async def convert_ardf(
+    ardf_path, conv_path=r"X:\Data\AFM\Cypher\ARDFtoHDF5.exe", force=False, pbar=None
+):
     """Turn an ARDF path into a corresponding HDF5 path, converting the file if it doesn't exist.
 
     Can force the conversion with the force flag if necessary (e.g. overwriting with new data).
     Requires converter executable available from Asylum Research"""
     ardf_path = trio.Path(ardf_path)
     #     conv_path = trio.Path(conv_path)
-    h5file_path = ardf_path.with_suffix('.h5')
+    h5file_path = ardf_path.with_suffix(".h5")
 
     if (not force) and (await h5file_path.is_file()):
         return h5file_path
 
     if pbar is None:
         import tqdm
-        pbar = tqdm.tqdm(total=100, unit='%', )
 
-    pbar.set_description('Converting ' + str(ardf_path))
+        pbar = tqdm.tqdm(total=100, unit="%",)
+
+    pbar.set_description("Converting " + str(ardf_path))
 
     async def reading_stdout():
         stdout = []
         async for bytes_ in proc.stdout:
             stdout.append(bytes_)
-        stdout = b''.join(stdout).decode()
+        stdout = b"".join(stdout).decode()
         print(stdout)
 
     async def reading_stderr():
         async for stuff in proc.stderr:
-            i = stuff.rfind(b'\x08') + 1  # first thing on right not a backspace
+            i = stuff.rfind(b"\x08") + 1  # first thing on right not a backspace
             most_recent_numeric_output = stuff[i:-1]  # crop % sign
             if most_recent_numeric_output:
                 pbar.update(float(most_recent_numeric_output.decode()) - pbar.n)
 
     try:
-        async with await trio.open_process([str(conv_path), str(ardf_path), str(h5file_path), ],
-                                           stderr=subprocess.PIPE, stdout=subprocess.PIPE,
-                                           ) as proc:
+        async with await trio.open_process(
+            [str(conv_path), str(ardf_path), str(h5file_path),],
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+        ) as proc:
             async with trio.open_nursery() as nursery:
                 nursery.start_soon(reading_stdout)
                 nursery.start_soon(reading_stderr)
     except FileNotFoundError as e:
-        print('Please ensure the full path to the Asylum converter tool ARDFtoHDF5.exe is in the conv_path argument',
-              flush=True)
+        print(
+            "Please ensure the full path to the Asylum converter tool ARDFtoHDF5.exe is in the conv_path argument",
+            flush=True,
+        )
         raise e
     finally:
         pbar.close()
@@ -94,9 +101,9 @@ NANOMETER_UNIT_CONVERSION = 1e9  # maybe we can intelligently read this from the
 
 class ForceMapWorker:
     def __init__(self, h5data):
-        self.force_curves = h5data['ForceMap']['0']
+        self.force_curves = h5data["ForceMap"]["0"]
         # ForceMap Segments can contain 3 or 4 endpoint indices for each indent array
-        self.segments = self.force_curves['Segments'][:, :, :]  # XXX Read h5data
+        self.segments = self.force_curves["Segments"][:, :, :]  # XXX Read h5data
         im_r, im_c, num_segments = self.segments.shape
 
         # Generally segments are [Ext, Dwell, Ret, Away] or [Ext, Ret, Away]
@@ -113,22 +120,25 @@ class ForceMapWorker:
         # We only care about 2 channels, Defl and ZSnsr
         # Convert channels array to a map that can be used to index into ForceMap data by name
         # chanmap should always be {'Defl':1,'ZSnsr':2} but it's cheap to calculate
-        self.chanmap = {key.decode('utf8'): index for index, key in enumerate(self.force_curves.attrs['Channels'])}
+        self.chanmap = {
+            key.decode("utf8"): index
+            for index, key in enumerate(self.force_curves.attrs["Channels"])
+        }
 
     def _shared_get_part(self, curve, s):
         # Index into the data and grab the Defl and Zsnsr ext and ret arrays as one 2D array
         # We could slice with "1:" if chanmap were constant but I'm not sure if it is
-        defl_zsnsr_rows = [self.chanmap['Defl'], self.chanmap['ZSnsr']]
+        defl_zsnsr_rows = [self.chanmap["Defl"], self.chanmap["ZSnsr"]]
         defl_zsnsr = curve[defl_zsnsr_rows, :]  # XXX Read h5data
 
         # we are happy to throw away data far from the surface to square up the data
         # Also reverse axis zero so data is ordered zsnsr,defl like we did for FFM
-        return defl_zsnsr[::-1, (s - self.minext):(s + self.minret)] * NANOMETER_UNIT_CONVERSION
+        return defl_zsnsr[::-1, (s - self.minext) : (s + self.minret)] * NANOMETER_UNIT_CONVERSION
 
     def get_force_curve(self, r, c):
         # Because of the nonuniform arrays, each indent gets its own dataset
         # indexed by 'row:column' e.g. '1:1'.
-        curve = self.force_curves[f'{r}:{c}']  # XXX Read h5data
+        curve = self.force_curves[f"{r}:{c}"]  # XXX Read h5data
         s = self.extlens[r, c]
 
         return (*self._shared_get_part(curve, s), self.minext)
@@ -138,13 +148,13 @@ class ForceMapWorker:
         x = np.empty((im_r, im_c, 2, self.minext + self.minret), dtype=np.float32)
         for index, curve in self.force_curves.items():
             # Unfortunately they threw in segments here too, so we skip over it
-            if index == 'Segments':
+            if index == "Segments":
                 continue
             _poll_for_cancel()
             # Because of the nonuniform arrays, each indent gets its own dataset
             # indexed by 'row:column' e.g. '1:1'. We could start with the shape and index
             # manually, but the string munging is easier for me to think about
-            r, c = index.split(':')
+            r, c = index.split(":")
             r, c = int(r), int(c)
             s = self.extlens[r, c]
 
@@ -163,7 +173,10 @@ class FFMSingleWorker:
         return z * NANOMETER_UNIT_CONVERSION, d * NANOMETER_UNIT_CONVERSION, len(z) // 2
 
     def get_all_curves(self, _poll_for_cancel=None):
-        return np.stack((self.drive, self.defl), axis=-2) * NANOMETER_UNIT_CONVERSION, self.drive.shape[-1] // 2
+        return (
+            np.stack((self.drive, self.defl), axis=-2) * NANOMETER_UNIT_CONVERSION,
+            self.drive.shape[-1] // 2,
+        )
 
 
 class FFMTraceRetraceWorker:
@@ -187,7 +200,10 @@ class FFMTraceRetraceWorker:
         _poll_for_cancel()
         defl = np.concatenate((self.defl_trace, self.defl_retrace))
         _poll_for_cancel()
-        return np.stack((drive, defl,), axis=-2) * NANOMETER_UNIT_CONVERSION, self.drive_trace.shape[-1] // 2
+        return (
+            np.stack((drive, defl,), axis=-2) * NANOMETER_UNIT_CONVERSION,
+            self.drive_trace.shape[-1] // 2,
+        )
 
 
 class AsyncARH5File:
@@ -199,11 +215,14 @@ class AsyncARH5File:
         h5data = await trs(h5py.File, self.h5file_path, "r")
         # The notes have a very regular key-value structure, so we convert to dict for later access
         self.notes = await trs(
-            lambda: dict(line.split(':', 1)
-                         for line in h5data.attrs["Note"].decode('utf8').split('\n')
-                         if ':' in line))
+            lambda: dict(
+                line.split(":", 1)
+                for line in h5data.attrs["Note"].decode("utf8").split("\n")
+                if ":" in line
+            )
+        )
         worker = await trs(self._choose_worker, h5data)
-        images, image_names = await trs(lambda: (h5data['Image'], list(h5data['Image'].keys())))
+        images, image_names = await trs(lambda: (h5data["Image"], list(h5data["Image"].keys())))
         self._h5data = h5data
         self._worker = worker
         self._images = images
@@ -221,24 +240,21 @@ class AsyncARH5File:
         await self.aclose()
 
     def _choose_worker(self, h5data):
-        if 'FFM' in h5data:
-            self.scandown = bool(self.notes['ScanDown'])
-            if '1' in h5data['FFM']:
-                worker = FFMTraceRetraceWorker(h5data["FFM"]["0"]["Drive"],
-                                               h5data["FFM"]["0"]["Defl"],
-                                               h5data["FFM"]["1"]["Drive"],
-                                               h5data["FFM"]["1"]["Defl"],
-                                               )
-            elif '0' in h5data["FFM"]:
-                worker = FFMSingleWorker(h5data["FFM"]["0"]["Drive"],
-                                         h5data["FFM"]["0"]["Defl"],
-                                         )
+        if "FFM" in h5data:
+            self.scandown = bool(self.notes["ScanDown"])
+            if "1" in h5data["FFM"]:
+                worker = FFMTraceRetraceWorker(
+                    h5data["FFM"]["0"]["Drive"],
+                    h5data["FFM"]["0"]["Defl"],
+                    h5data["FFM"]["1"]["Drive"],
+                    h5data["FFM"]["1"]["Defl"],
+                )
+            elif "0" in h5data["FFM"]:
+                worker = FFMSingleWorker(h5data["FFM"]["0"]["Drive"], h5data["FFM"]["0"]["Defl"],)
             else:
-                worker = FFMSingleWorker(h5data["FFM"]["Drive"],
-                                         h5data["FFM"]["Defl"],
-                                         )
+                worker = FFMSingleWorker(h5data["FFM"]["Drive"], h5data["FFM"]["Defl"],)
         else:
-            self.scandown = bool(self.notes['FMapScanDown'])
+            self.scandown = bool(self.notes["FMapScanDown"])
             worker = ForceMapWorker(h5data)
         return worker
 
