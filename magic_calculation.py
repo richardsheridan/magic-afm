fix calculated image map units

magic_calculation.py
@@ -555,11 +555,11 @@ def calc_properties_imap(delta_f_i, **kwargs):
     if np.all(np.isfinite(beta)):
         (deflection, indentation, z_true_surface,) = calc_def_ind_ztru(f, beta, **kwargs)
         properties = (
-            beta[0],
-            -beta[1],
-            deflection,
-            indentation,
-            -z_true_surface,
+            beta[0]*1e9,
+            -beta[1]/1e9,
+            deflection/1e9,
+            indentation/1e9,
+            -z_true_surface/1e9,
             deflection / indentation,
         )
     else:

magic_gui.py
@@ -595,11 +595,11 @@ class ARDFWindow:
                 raise ValueError("Unknown fit_mode: ", options.fit_mode)
 
             property_names_units = {
-                "CalcIndentationModulus": "GPa",
-                "CalcAdhesionForce": "nN",
-                "CalcDeflection": "nm",
-                "CalcIndentation": "nm",
-                "CalcTrueHeight": "nm",  # Hi z -> lo h
+                "CalcIndentationModulus": "Pa",
+                "CalcAdhesionForce": "N",
+                "CalcDeflection": "m",
+                "CalcIndentation": "m",
+                "CalcTrueHeight": "m",  # Hi z -> lo h
                 "CalcIndentationRatio": "",
             }
             property_map = np.empty(