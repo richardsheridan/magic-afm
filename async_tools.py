a631e8a947e9d3d703b7500f1dfa18427e8fb9b2
Richard Sheridan <richard.sheridan@gmail.com>
2020-07-30 13:50:59 -0400

    don't include sleep time in thread map delay

--- async_tools.py
+++ async_tools.py
@@ -370,10 +370,9 @@ async def thread_map(sync_fn, job_items, *args, cancellable=True, limiter=cpu_bo
     async with trio.open_nursery() as nursery:
         t = trio.current_time()
         for i, item in enumerate(job_items):
-            t1 = trio.current_time()
-            if t1-t > LONGEST_IMPERCEPTIBLE_DELAY/2:
+            if trio.current_time()-t > LONGEST_IMPERCEPTIBLE_DELAY/2:
                 await trio.sleep(0)
-                t = t1
+                t = trio.current_time()
             nursery.start_soon(thread_worker, item, i, send_chan.clone())
         await send_chan.aclose()
         async for i, result in recv_chan:
