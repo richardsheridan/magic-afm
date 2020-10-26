32eabe28f634933f0db36901e6b5780bf7862fc7
richardsheridan <richard.sheridan@gmail.com>
2020-10-26 10:57:20 -0400

    let executor shut itself down

--- magic_afm/gui.py
+++ magic_afm/gui.py
@@ -1638,8 +1638,6 @@ async def main_task(root):
         menu_frame.add_cascade(label="Help", menu=help_menu, underline=0)
 
         await trio.sleep_forever()  # needed if nursery never starts a long running child
-    if async_tools.EXECUTOR is not None:
-        async_tools.EXECUTOR.shutdown(wait=True)
 
 
 def main():
