6abe7eab0cfa80fe39b7cd12a6e5303d8cfb2dc3
richardsheridan <richard.sheridan@gmail.com>
2022-02-14 11:55:36 -0500

    more principled build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
 @echo off
-call %USERPROFILE%\Miniconda3\Scripts\activate.bat %USERPROFILE%\Miniconda3\envs\mafm-build\
+call %USERPROFILE%\Miniconda3\Scripts\activate.bat
+call conda create -n mafm-build-tmp -y python=3.9
+call conda activate mafm-build-tmp
+pip install -U -r requirements.txt
 python magic_afm\make_version.py
 pyinstaller -y magic_gui_win.spec
+call conda deactivate
+call conda remove -n mafm-build-tmp -y --all
