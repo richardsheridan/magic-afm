666f5b3f638983ea5a3c37887c6bd41739b408c2
richardsheridan <richard.sheridan@gmail.com>
2022-04-15 15:25:51 -0400

    easy test with latest matplotlib

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@ call %USERPROFILE%\Miniconda3\Scripts\activate.bat
 call conda create -n mafm-build-tmp -y python=3.10
 call conda activate mafm-build-tmp
 pip install -U -r requirements.txt
+REM pip install -U -r requirements-nightly-mpl.txt
 python magic_afm\make_version.py
 pyinstaller -y magic_gui_win.spec
 call conda deactivate
