9ca70ad7a3eec62fe21e25563cfd351467d8c1d6
richardsheridan <richard.sheridan@gmail.com>
2022-11-21 11:41:52 -0500

    fix numpy/numba threads in frozen workers

--- magic_gui.py
+++ magic_gui.py
@@ -1,11 +1,13 @@
 # Entrypoint for pyinstaller
 
+import os
+
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+
 if __name__ == '__main__':
     import multiprocessing
-    multiprocessing.freeze_support()
+    multiprocessing.freeze_support()  # frozen workers run HERE then raise SystemExit
+    del os.environ["OPENBLAS_NUM_THREADS"]
     from magic_afm.gui import main
     main()
-else:
-    import os
-    os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
